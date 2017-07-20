66edd9594fce3aa7a0c989138f07b37fdecbf299
crosalot <crosalot#gmail.com>
2017-07-20 11:48:23 +0700

    add place admin

--- areas/admin.py
+++ areas/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from areas.models import PlaceCategory, Place
+
+
+class PlaceCategoryAdmin(admin.ModelAdmin):
+    pass
+
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PlaceCategory, PlaceCategoryAdmin)
+admin.site.register(Place, PlaceAdmin)
