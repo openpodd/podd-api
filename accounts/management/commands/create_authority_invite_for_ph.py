552472c121005cbd727434ec506bc1093602f8b9
polawat phetra <pphetra@gmail.com>
2019-03-06 20:57:07 +0700

    Scripts for create invitation code for public health.

---
+++ accounts/management/commands/create_authority_invite_for_ph.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from accounts.models import Authority, AuthorityInvite
+from common.constants import USER_STATUS_PUBLIC_HEALTH
+from common.functions import get_data_from_csv
+from reports.models import AdministrationArea
+
+
+class Command(BaseCommand):
+    args = 'file_path.csv domain_id'
+    help = 'Import Authority with code from csv: file_path.csv'
+
+    def handle(self, *args, **options):
+
+        if not args:
+            raise CommandError('Please provide domain_id')
+
+        domain_id = args[0]
+        settings.CURRENT_DOMAIN_ID = domain_id
+
+        # first pass
+        for authority in Authority.objects.filter(domain_id=domain_id):
+            if not AuthorityInvite.objects.filter(status=USER_STATUS_PUBLIC_HEALTH, authority=authority, disabled=False).exists():
+                print 'create invite for %s' % (authority.code,)
+                ph_invite = AuthorityInvite(
+                    authority=authority,
+                    status=USER_STATUS_PUBLIC_HEALTH
+                )
+                ph_invite.save()
