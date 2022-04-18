db9b6a5be3392e281b61fe2aac0488201c6d26e9
pphetra <pphetra@gmail.com>
2022-04-18 12:57:51 +0700

    create default user command for cross domain reporting.

---
+++ accounts/management/commands/create_cross_domain_user.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+import time
+from django.core.management import BaseCommand
+from django.utils.log import getLogger
+
+from accounts.models import User
+from common.models import Domain
+
+
+class Command(BaseCommand):
+    help = 'Create default user for cross domain report'
+
+    def handle(self, *args, **options):
+        logger = getLogger('management_commands')
+
+        for domain in Domain.objects.all():
+            username = 'public-%s' % (domain.id,)
+            try:
+                user = User.default_manager.get(username=username)
+            except User.DoesNotExist:
+                print("create user %s for domain %s" % (username, domain.name))
+                User.default_manager.create(
+                    username=username,
+                    domain=domain,
+                    first_name=u"รายงานข้ามจังหวัด",
+                    last_name="",
+                    password=time.time()
+                )
